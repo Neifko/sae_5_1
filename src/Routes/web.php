72e2d2b0ed0f09b10f44f0f39826013314ce88c3
xavierli901 <xavierli901@gmail.com>
2024-12-16 10:46:31 +0100

    Ajout de la route pour le module sous réseau

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use Victor\Sae51\Controllers\HomeController;
+use Victor\Sae51\Controllers\ModuleController;
+
 
 $router = new AltoRouter();
 
 
 $router->map('GET', '/', [HomeController::class, 'index'], 'home');
 $router->map('GET', '/test', [HomeController::class, 'test'], 'test');
+
+$router->map('GET', '/module_sousreseau', [ModuleController::class, 'module_sousreseau'], 'module_sousreseau');
 
 $router->map('GET', '/protected', [HomeController::class, 'protected_route'], 'protected');
 
