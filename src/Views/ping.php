d5d6f4b74b637e94805d0633cfd16ba93d13c84f
Yassine ELKHALKI <yassine.elkhalki@outlook.fr>
2024-12-16 22:48:59 +0100

    modify form

--- src/Views/ping.php
+++ src/Views/ping.php
@@ -8,10 +8,9 @@
 <body>
     <h1>Ping avec Scapy</h1>
 
-    <!-- Formulaire pour entrer l'adresse IP ou l'URL -->
     <form action="/ping" method="post">
         <label for="host">Adresse IP ou URL :</label>
-        <input type="text" id="host" name="host" required>
+        <input type="text" id="host" name="host" placeholder="www.free.fr" required>
         <button type="submit">Ping</button>
     </form>
 
