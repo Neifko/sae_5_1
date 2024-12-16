baf157e3a7d267e32dfbce94cab883d7344c557a
ostacheGIT <ostap.tymchyshyn.92@gmail.com>
2024-12-16 14:40:53 +0100

    feat : Ajout d'une validation complète pour les adresses IPv6
    Ajout d'une nouvelle fonction `verifyIPv6` avec un regex amélioré qui prend en charge :
    - Les adresses IPv6 complètes.
    - Les adresses IPv6 compressées avec `::`.
    - Les adresses de lien local (par ex. `fe80::1%lo0`).
    - Les adresses IPv4 intégrées (par ex. `::ffff:192.0.2.128`).

--- public/javascript/classe_ipv6.js
+++ public/javascript/classe_ipv6.js
@@ -1,1 +1,20 @@
-//TODO
+function verifyIPv6(ipv6) {
+    const ipv6Regex = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/;
+    return ipv6Regex.test(ipv6);
+}
+
+document.getElementById('simplifier-button').addEventListener('click', function (event) {
+    event.preventDefault();
+    var ipv6 = document.getElementById('ipv6').value;
+    const resultDiv = document.getElementById('resultDiv');
+    if (verifyIPv6(ipv6)) {
+        resultDiv.innerHTML = `<p> bon</p>`;
+    } else {
+        resultDiv.innerHTML = `<p> pas bon </p>`;
+    }
+});
+
+document.getElementById('etendre-button').addEventListener('click', function (event) {
+    event.preventDefault();
+    resultDiv.innerHTML = `<p> cliqué </p>`;
+});
--- src/Views/ipv6.php
+++ src/Views/ipv6.php
@@ -9,11 +9,11 @@
     <form id="ipv6-form">
         <label for="ipv6">Entrez une adresse IPv6 :</label>
         <input type="text" id="ipv6" placeholder="ex : 2001:db8::ff00:42:8329" required>
-        <button type="submit" id="simplifier_button">Simplifier</button>
-        <button type="submit" id="etendre_button">Etendre</button>
+        <button type="submit" id="simplifier-button">Simplifier</button>
+        <button type="submit" id="etendre-button">Etendre</button>
     </form>
     
-    <div id="result"></div>
+    <div id="resultDiv"></div>
 
     <script src="/javascript/classe_ipv6.js"></script>
 </body>
