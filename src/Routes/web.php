28c37cb2326c9010d69fc8e627c318829befe955
xavierli901 <xavierli901@gmail.com>
2024-12-17 10:30:02 +0100

    Ajout d'un commentaire

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -12,6 +12,7 @@ $router = new AltoRouter();
 $router->map('GET', '/', [HomeController::class, 'index'], 'home');
 $router->map('GET', '/test', [HomeController::class, 'test'], 'test');
 
+// Route module_sousreseau
 $router->map('GET', '/module_sousreseau', [ModuleController::class, 'module_sousreseau'], 'module_sousreseau');
 
 $router->map('GET', '/protected', [HomeController::class, 'protected_route'], 'protected');
