4eb7dca4354e31bb314ec49f03a5db3ef065170b
Alexis <alexie78180@gmail.com>
2024-12-18 16:27:51 +0100

    feat(18):Style
    Mise en place de l'header, du footer et de l'index pourle site

--- src/Views/partials/footer.php
+++ src/Views/partials/footer.php
@@ -1,8 +1,6 @@
-<link rel="stylesheet" href="/css/global.css">
-
-
+<hr>
 <footer>
-    <hr>
+
     <p>&copy; 2024 - Procrastinateurs Team. Tous droits réservés.</p>
     <p>Par Victor G, Alexis L, Ostap T, Yassine E et Xavier L</p>
 </footer>
