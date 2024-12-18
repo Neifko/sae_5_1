5b41df419369b0dee16ac14c54395ae06fcfaa1c
Alexis <alexie78180@gmail.com>
2024-12-18 13:37:31 +0100

    feat(18):style
    home.php resolved
    
    # Conflicts:
    #	src/Views/pages/home.php
    
    # Conflicts:
    #	src/Views/pages/home.php

--- src/Views/pages/home.php
+++ src/Views/pages/home.php
@@ -1,9 +1,32 @@
-
-<h1>Accueil</h1>
-<p>Ceci est une page générée avec PHP.</p>
-<div>
-    <ul>
-        <li><a href="/register">Se créer un compte</a> </li>
-        <li><a href="/login">Se connecter</a> </li>
-    </ul>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Application Web Pédagogique</title>
+</head>
+<body>
+<div class="header">
+    <img src="./images/logo.png" alt="logo procrastinateurs">
+    <h1>Application Web Pédagogique</h1>
+    <div>
+        <img src="./images/user-interface.png" alt="User interface logo">
+        <p><a href="./login">Se connecter</a>/<a href="./register">S'inscrire</a></p>
+    </div>
 </div>
+<hr>
+
+<div class="imgAccueil">
+    <img src="./images/imageAccueil1.jpg" alt="images">
+
+</div>
+
+<footer>
+    <hr>
+    <div>
+        Site créé par Victor, Yassine, Alexis, Xavier et Ostap
+    </div>
+</footer>
+</body>
+</html>
