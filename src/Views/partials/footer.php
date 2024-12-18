6e4888f960f8ff28f8b8e5c1c2eb41e539208c75
Alexis <alexie78180@gmail.com>
2024-12-18 16:27:51 +0100

    feat(18):Style
    Mise en place de l'header, du footer et de l'index pourle site

--- src/Views/partials/footer.php
+++ src/Views/partials/footer.php
@@ -1,10 +1,8 @@
-<link rel="stylesheet" href="/css/global.css">
-
-
+<hr>
 <footer>
-    <hr>
-    <p>&copy; 2024 - Procrastinateurs Team. Tous droits réservés.
-    Par Victor G, Alexis L, Ostap T, Yassine E et Xavier L</p>
+
+    <p>&copy; 2024 - Procrastinateurs Team. Tous droits réservés.</p>
+    <p>Par Victor G, Alexis L, Ostap T, Yassine E et Xavier L</p>
 </footer>
 </body>
 </html>
--- src/Views/partials/header.php
+++ src/Views/partials/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
-    <link rel="stylesheet" href="/css/global.css">
-    <link rel="stylesheet" href="/css/header.css">
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/header.css">
 </head>
 <body>
 
@@ -16,12 +16,5 @@
     <div>
         <h1>Application Web Pédagogique</h1>
     </div>
-    <?php if (!preg_match('/^(\/|\/dashboard)$/', $_SERVER['REQUEST_URI'])): ?>
-        <div id="boutonRetourHeader">
-            <button>
-                <a href="/">Retour</a>
-            </button>
-        </div>
-    <?php endif; ?>
 </div>
 <hr>
