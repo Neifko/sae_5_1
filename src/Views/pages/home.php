73ba86d81b99bf09d865979ad791c126741e0b49
Alexis <alexie78180@gmail.com>
2024-12-18 16:27:51 +0100

    feat(18):Style
    Mise en place de l'header, du footer et de l'index pourle site

--- src/Views/pages/home.php
+++ src/Views/pages/home.php
@@ -1,32 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Application Web Pédagogique</title>
-</head>
-<body>
-<div class="header">
-    <img src="./images/logo.png" alt="logo procrastinateurs">
-    <h1>Application Web Pédagogique</h1>
-    <div>
-        <img src="./images/user-interface.png" alt="User interface logo">
-        <p><a href="./login">Se connecter</a>/<a href="./register">S'inscrire</a></p>
-    </div>
-</div>
-<hr>
+<link rel="stylesheet" href="./css/index.css">
+<link rel="stylesheet" href="./css/global.css">
+<title>Application Web Pédagogique</title>
+
+<p class="login_signin">
+    <a href="./login">Se connecter</a>
+    /
+    <a href="./register">S'inscrire</a>
+</p>
+
 
 <div class="imgAccueil">
     <img src="./images/imageAccueil.jpg" alt="images">
-
-</div>
-
-<footer>
-    <hr>
-    <div>
-        Site créé par Victor, Yassine, Alexis, Xavier et Ostap
-    </div>
-</footer>
-</body>
-</html>
+</div
--- src/Views/partials/header.php
+++ src/Views/partials/header.php
@@ -4,5 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/header.css">
 </head>
 <body>
+
+<div class="header">
+    <div>
+        <img src="./images/logo.png" alt="logo procrastinateurs">
+    </div>
+    <div>
+        <h1>Application Web Pédagogique</h1>
+    </div>
+</div>
+<hr>
