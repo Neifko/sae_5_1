5c50b2012a44804abbbd729df6a82175d9ff9e6d
Junior 78 <alexie78180@gmail.com>
2024-12-18 20:58:09 +0100

    feat(18):Style
    Ajout des styles pour les onglets, reste encore à bien placer les liens en plus beau

--- src/Views/partials/footer.php
+++ src/Views/partials/footer.php
@@ -1,6 +1,8 @@
-<hr>
-<footer>
+<link rel="stylesheet" href="/css/global.css">
 
+
+<footer>
+    <hr>
     <p>&copy; 2024 - Procrastinateurs Team. Tous droits réservés.</p>
     <p>Par Victor G, Alexis L, Ostap T, Yassine E et Xavier L</p>
 </footer>
