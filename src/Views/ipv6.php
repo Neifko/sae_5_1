966399bfb214de93815b9d7ae317dc7d2c07b23f
ostacheGIT <ostap.tymchyshyn.92@gmail.com>
2024-12-18 14:28:53 +0100

    feat: Ajout de la détection des adresses locales uniques (LUA), glabales et link-local.
    Ajout de la fonction qui a pour logique l'identification des adresses IPv6 locales uniques (fd00::/8), globales ou link-local. Les autres adresses seront classés non classifiables ou autre.

--- src/Views/ipv6.php
+++ src/Views/ipv6.php
@@ -11,6 +11,7 @@
         <input type="text" id="ipv6" placeholder="ex : 2001:db8::ff00:42:8329" required>
         <button type="submit" id="simplifier-button">Simplifier</button>
         <button type="submit" id="etendre-button">Etendre</button>
+        <button id="classify-button">Classifier</button>
     </form>
     
     <div id="resultDiv"></div>
