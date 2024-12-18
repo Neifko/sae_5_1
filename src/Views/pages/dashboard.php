4240f3c1127093e995cd64fa09b9104a88535f6a
Alexis <alexie78180@gmail.com>
2024-12-18 16:27:51 +0100

    feat(18):Style
    Mise en place de l'header, du footer et de l'index pourle site

--- src/Views/pages/dashboard.php
+++ src/Views/pages/dashboard.php
@@ -1,35 +1,25 @@
-<link rel="stylesheet" href="/css/dashboard.css">
+<link rel="stylesheet" href="./css/dashboard.css">
+<link rel="stylesheet" href="./css/global.css">
 
-<div class="profil">
+    <h2>Vous êtes connecté</h2>
+    <ul>
+        <li><a href="/logout">Se déconnecter</a> </li>
+        <li><a href="/profile/<?= htmlspecialchars($user_id) ?>">Profil</a> </li>
+        <li><a href="/list-users">Liste des utilisateurs</a> </li>
+    </ul>
+
     <div>
-        <h2>Vous êtes connecté</h2>
-    </div>
-    <div>
-        <a href="/profile/<?= htmlspecialchars($user_id) ?>">
-            <img src="./images/user-interface.png" alt="image Profil">
-            Profil
-        </a>
+        <?php if (isset($_SESSION['flash_message'])): ?>
+            <?php echo htmlspecialchars($_SESSION['flash_message']['content']); ?>
+            <?php unset($_SESSION['flash_message']); ?>
+        <?php endif; ?>
     </div>
 
-</div>
+    <ul>
 
-<ul>
-
-    <li><a href="/list-users">Liste des utilisateurs</a></li>
-</ul>
-
-<div>
-    <?php if (isset($_SESSION['flash_message'])): ?>
-        <?php echo htmlspecialchars($_SESSION['flash_message']['content']); ?>
-        <?php unset($_SESSION['flash_message']); ?>
-    <?php endif; ?>
-</div>
-
-<ul>
-
-    <li><a href="/module_traduction">Module de Traduction IPV4</a></li>
-    <li><a href="/module_sousreseau">Module sous réseau</a></li>
-    <li><a href="/ipv4">IPv4</a></li>
-    <li><a href="/ipv6">IPv6</a></li>
-    <li><a href="/scapy">Module Scapy</a></li>
-</ul>
+        <li><a href="/module_traduction">Module de Traduction IPV4</a></li>
+        <li><a href="/module_sousreseau">Module sous réseau</a> </li>
+        <li><a href="/ipv4">IPv4</a> </li>
+        <li><a href="/ipv6">IPv6</a> </li>
+        <li><a href="/scapy">Module Scapy</a> </li>
+    </ul>
