d723b1646cc1ae0379f548a3e8462b8b3a92f06c
ostacheGIT <ostap.tymchyshyn.92@gmail.com>
2024-12-19 09:31:41 +0100

    fix : modification CSS dashboard
    le fichier dashboard.css et dashboard.php ont été modifiés  pou run meilleur style css.

--- src/Views/pages/dashboard.php
+++ src/Views/pages/dashboard.php
@@ -1,35 +1,53 @@
 <link rel="stylesheet" href="/css/dashboard.css">
 
-<div class="profil">
-    <div>
-        <h2>Vous êtes connecté</h2>
+<div class="dashboard-container">
+    <!-- Colonne de gauche -->
+    <div class="left-column">
+        <div class="dashboard-block profil">
+            <h2>Vous êtes connecté</h2>
+            <a href="/profile/<?= htmlspecialchars($user_id) ?>">
+                <img src="./images/user-interface.png" alt="Image Profil">
+                Profil
+            </a>
+        </div>
+
+        <div class="dashboard-block flash-message">
+            <?php if (isset($_SESSION['flash_message'])): ?>
+                <?= htmlspecialchars($_SESSION['flash_message']['content']) ?>
+                <?php unset($_SESSION['flash_message']); ?>
+            <?php endif; ?>
+        </div>
     </div>
-    <div>
-        <a href="/profile/<?= htmlspecialchars($user_id) ?>">
-            <img src="./images/user-interface.png" alt="image Profil">
-            Profil
-        </a>
+
+    <!-- Colonne de droite -->
+    <div class="right-column">
+        <div class="modules-grid">
+            <!-- Modules -->
+            <div class="dashboard-block">
+                <ul class="dashboard-links">
+                    <li><a href="/module_traduction">Module de Traduction IPV4</a></li>
+                </ul>
+            </div>
+            <div class="dashboard-block">
+                <ul class="dashboard-links">
+                    <li><a href="/module_sousreseau">Module sous réseau</a></li>
+                </ul>
+            </div>
+            <div class="dashboard-block">
+                <ul class="dashboard-links">
+                    <li><a href="/ipv4">IPv4</a></li>
+                </ul>
+            </div>
+            <div class="dashboard-block">
+                <ul class="dashboard-links">
+                    <li><a href="/ipv6">IPv6</a></li>
+                </ul>
+            </div>
+            <div class="dashboard-block">
+                <ul class="dashboard-links">
+                    <li><a href="/scapy">Module Scapy</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
-
 </div>
-
-<ul>
-
-    <li><a href="/list-users">Liste des utilisateurs</a></li>
-</ul>
-
-<div>
-    <?php if (isset($_SESSION['flash_message'])): ?>
-        <?php echo htmlspecialchars($_SESSION['flash_message']['content']); ?>
-        <?php unset($_SESSION['flash_message']); ?>
-    <?php endif; ?>
-</div>
-
-<ul>
-
-    <li><a href="/module_traduction">Module de Traduction IPV4</a></li>
-    <li><a href="/module_sousreseau">Module sous réseau</a></li>
-    <li><a href="/ipv4">IPv4</a></li>
-    <li><a href="/ipv6">IPv6</a></li>
-    <li><a href="/scapy">Module Scapy</a></li>
-</ul>
