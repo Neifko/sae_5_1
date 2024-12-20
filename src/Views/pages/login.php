b661b97caa94e57cb2b85707342a089f3e4cf1ab
xavierli901 <xavierli901@gmail.com>
2024-12-20 09:08:13 +0100

    feat : implémentation de sweet alert pour la page de connexion, la liste des utilisateurs et le dashboard

--- src/Views/pages/login.php
+++ src/Views/pages/login.php
@@ -7,7 +7,13 @@
 
     <div>
         <?php if (isset($_SESSION['flash_message'])): ?>
-            <?php echo htmlspecialchars($_SESSION['flash_message']['content']); ?>
+            <script>
+                Swal.fire({
+                    icon: "<?php echo htmlspecialchars($_SESSION['flash_message']['type']); ?>",
+                    type: "<?php echo htmlspecialchars($_SESSION['flash_message']['type']); ?>", // 'success', 'error', etc.
+                    text: "<?php echo htmlspecialchars($_SESSION['flash_message']['content']); ?>"
+                })
+            </script>
             <?php unset($_SESSION['flash_message']); ?>
         <?php endif; ?>
     </div>
